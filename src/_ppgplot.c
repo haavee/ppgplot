3f21862c0eca25a18e0b90545d2118f38861315c
Marjolein Verkouter <verkouter@jive.eu>
2025-08-12 17:00:12 +0200

    Indicate LGPL, update contributors in pyproject
    Time to update some of the meta information about who contributed and
    make explicit what the license of the code is (LGPL).
    
    The LGPL is mentioned in PKG-INFO but no-one (recent) will look into
    that.

--- src/_ppgplot.c
+++ src/_ppgplot.c
@@ -1,3 +1,19 @@
+/* Copyright (c) 1999-2025 N Patavalis, MA Breddels, C Bassa, M Verkouter
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see
+ * <https://www.gnu.org/licenses/>.
+*/
 /*
  * FILE:
  *    _ppgplot.c
@@ -14,6 +30,7 @@
  *    - The pythonic calling conventions of some functions are *not*
  *      identical to the original PGPLOT ones.
  */
+
 
 #include <Python.h>
 
