f3081f14e93a01bd0bb38e61cf4dc81b271f0e4f
Marjolein Verkouter <verkouter@jive.eu>
2025-11-09 22:20:28 +0100

    Remove bld.bat (conda-forge review)
    github conda-forge reviewer suggested that if windows builds are skipped
    anyway, why not loose bld.bat?
    Excellent suggestion, thx.

--- conda-recipe/bld.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Windows build script - currently not supported due to giza dependency
-echo "Windows builds are not currently supported for python-pgplot"
-echo "This is due to the giza dependency not being available on Windows"
-exit /b 1
